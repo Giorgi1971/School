667ce2e2922ec55ad2a06543924afe6e33377ea9
gio1971 <giooorgi7@gmail.com>
2021-10-16 23:42:01 +0400

    first commit

--- build/models.py
+++ build/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Subject(models.IntegerChoices):
+    PHYSIC = 1
+    MUSIC = 2
+    CHEMYSTRY = 3
+
+
+class Room(models.Model):
+    number = models.PositiveSmallIntegerField()
--- build/views.py
+++ build/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.http.response import HttpResponse
+from django.shortcuts import get_list_or_404, render
+from .models import Room
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'base.html')
+
+
+def room(request):
+    room_list = get_list_or_404(Room)
+    return render(request, 'base.html', {'room_list':room_list})
